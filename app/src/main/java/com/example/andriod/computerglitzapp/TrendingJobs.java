a44c2c583bc2fb42b2dff0aaa15919c402b625ae
Nora Dixon <noradixon03@gmail.com>
2018-04-15 17:33:57 -0500

    What is up with this

--- app/src/main/java/com/example/andriod/computerglitzapp/TrendingJobs.java
+++ app/src/main/java/com/example/andriod/computerglitzapp/TrendingJobs.java
@@ -7,6 +7,8 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.MenuItem;
 import android.widget.TextView;
 
@@ -19,6 +21,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class TrendingJobs extends AppCompatActivity {
+
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -52,19 +58,29 @@ public class TrendingJobs extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_trending_jobs);
+        mRecyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
+        mRecyclerView.setHasFixedSize(true);
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        mAdapter = new MyAdapter(myDataset);
+        mRecyclerView.setAdapter(mAdapter);
+
         int row = 0;
-        while (row<46) {
+        while (row < 46) {
             TrendingJobsLoader initialLoader = new TrendingJobsLoader();
             List<TrendingJobsData> Jobs = initialLoader.loadTrendingJobsData(TrendingJobs.this);
+            List<TrendingJobsData> Pay = initialLoader.loadTrendingJobsData(TrendingJobs.this);
             final TrendingJobsData trendingJobsDataList = Jobs.get(row);
-            if (row==0) {
+            // final TrendingJobsData trendingJobsDataList = Pay.get(row);
+            if (row == 0) {
                 TextView mJob1 = (TextView) findViewById(R.id.Job1);
                 mJob1.setText(trendingJobsDataList.getJob());
             }
             row++;
-            }
+        }
 
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
-        }
-    }
+
+    }}
+
