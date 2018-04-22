fceb01dd0ec0028fb680de1031075b52198b5c40
Clarekpd <Clarekpd@outlook.com>
2018-04-22 16:12:46 -0500

    :)

---
+++ app/src/main/java/com/example/andriod/computerglitzapp/FinancailInfoAdapter.java
@@ -0,0 +1,48 @@
+package com.example.andriod.computerglitzapp;
+
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.RecyclerView.Adapter;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.TextView;
+
+/**
+ * Created by Clare on 4/7/2018.
+ */
+
+public class FinancailInfoAdapter extends Adapter<FinancailInfoAdapter.MyViewHolder> {
+static class MyViewHolder extends RecyclerView.ViewHolder{
+    private TextView mMytextview;
+    private CheckBox mMainCheckbox;
+    private CheckBox mCollapsableCheckbox;
+
+    private int mIndent;
+    public MyViewHolder(View itemView) {
+        super(itemView);
+        mMytextview = itemView.findViewById(R.id.view_holder_textview);
+        mMainCheckbox = itemView.findViewById(R.id.view_holder_checkbox);
+        mCollapsableCheckbox = itemView.findViewById(R.id.view_holder_collapeser_checkbox);
+        mIndent = (int) itemView.getContext().getResources().getDimension(R.dimen.activity_checklist_indent);
+    }
+}
+
+    @Override
+    public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View root = LayoutInflater.from(parent.getContext()).inflate(R.layout.viewholder_checklist_item, parent, false);
+        return new MyViewHolder(root);
+    }
+
+    @Override
+    public void onBindViewHolder(MyViewHolder holder, int position) {
+ChecklistItem item = ChecklistData.mChecklist.get(position);
+holder.mMytextview.setText(item.getmText());
+holder.itemView.setPadding(holder.mIndent*item.getmIndent(),0,0,0);
+    }
+
+    @Override
+    public int getItemCount() {
+        return ChecklistData.mChecklist.size();
+    }
+}
---
+++ app/src/main/java/com/example/andriod/computerglitzapp/FinancailInfoData.java
@@ -0,0 +1,55 @@
+package com.example.andriod.computerglitzapp;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Clare on 4/7/2018.
+ */
+
+public class FinancailInfoData {
+    public static ArrayList<ChecklistItem> mChecklist = new ArrayList<>();
+    static {
+        mChecklist.add(new ChecklistItem(0, "Getting Started", false));
+        mChecklist.add(new ChecklistItem(1, "Career Planning", false));
+        mChecklist.add(new ChecklistItem(2, "Explore these links to think about your interests, skills, and careers \n" +
+                "-Career Exploration - link https://www.bls.gov/k12/content/students/careers/career-exploration.htm \n" +
+                "- Career assessment tools - link https://www.monster.com/career-advice/article/best-free-career-assessment-tools \n" +
+                "- MnCareers Interest Assessment tool - https://mygpslifeplan.org/app/interest-assessment \n" +
+                "- My Next Move - Interest Profiler - https://www.mynextmove.org/explore/ip \n" +
+                "- Career Interest Explorer - link https://careerwise.minnstate.edu/careers/interestassessment.html  \n" +
+                "- GPS LifePlan - https://mygpslifeplan.org/app/interest-assessment \n" +
+                "-CAREERwise Assessment options by Minnesota State ISEEK - http://www.iseek.org/careers/stem.html  \n", false));
+        mChecklist.add(new ChecklistItem(2, "-Talk with your parents/counselor/trusted adult\n", false));
+        mChecklist.add(new ChecklistItem(2, "Job Shadow", false));
+        mChecklist.add(new ChecklistItem(1, "Document your progress and highlight your achievements - large edit field with bullets for each item - Plus sign to add more items with large edit field\n", false));
+        mChecklist.add(new ChecklistItem(2, "Clubs/Activities/n", false));
+        mChecklist.add(new ChecklistItem(2, "Grades/School Work/n", false));
+        mChecklist.add(new ChecklistItem(2, "Volunteering/n", false));
+        mChecklist.add(new ChecklistItem(2, "Work Experience/n", false));
+        mChecklist.add(new ChecklistItem(2, "Leadership Roles/n", false));
+        mChecklist.add(new ChecklistItem(2, "Specialized Academic Acheivements/n", false));
+        mChecklist.add(new ChecklistItem(2, "Other information that shows your interests(ex. projects, things you have built, art, music)/n", false));
+        mChecklist.add(new ChecklistItem(1, "Favorite Schools That Have Hands On Experience/n", false));
+        mChecklist.add(new ChecklistItem(1, "Things To Know/n", false));
+        mChecklist.add(new ChecklistItem(1, "-Do you want to transfer to a 4-year school?/n" +
+                "\t-Determine your 4-year school choice: many 4-year colleges only accept transfers from specific community colleges/n", false));
+        mChecklist.add(new ChecklistItem(2, "Check for articulation agreement/n", false));
+        mChecklist.add(new ChecklistItem(1, "Search for schools using the hands on app/n", false));
+        mChecklist.add(new ChecklistItem(1, "Explore selected schools in detail/n", false));
+        mChecklist.add(new ChecklistItem(2, "Go to hands on [favorites]/n", false));
+        mChecklist.add(new ChecklistItem(3, "Check information/n", false));
+        mChecklist.add(new ChecklistItem(3, "Complete school checklist/n", false));
+        mChecklist.add(new ChecklistItem(1, "Check for finacial aid/n", false));
+        mChecklist.add(new ChecklistItem(2, "Use [resources] option/n", false));
+        mChecklist.add(new ChecklistItem(2, "FAFSA link https://fafsa.ed.gov/ /n", false));
+        mChecklist.add(new ChecklistItem(2, "Prepare in advance - this can be a long, but valuable process - Get tax and investment documents, Social Security card or other government ID, and other income/expense documents in order so they are ready when applying\n/n", false));
+        mChecklist.add(new ChecklistItem(2, "Submit FASFA/n", false));
+        mChecklist.add(new ChecklistItem(2, "https://www.scholarships.com/ \n/n", false));
+        mChecklist.add(new ChecklistItem(2, "Work with school counselors to learn about more scholarship and grant opportunities\n/n", false));
+        mChecklist.add(new ChecklistItem(1, "Complete application process/n", false));
+        mChecklist.add(new ChecklistItem(2, "Get copies of HS Diploma, transcript, or GED/n", false));
+        mChecklist.add(new ChecklistItem(2, "Go to [Favorites] in Hands On app/n", false));
+        mChecklist.add(new ChecklistItem(3, "Complete school specific information/n", false));
+        mChecklist.add(new ChecklistItem(1, "Complete school checklist/n", false));
+    }
+    }
