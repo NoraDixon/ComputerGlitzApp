357f314abd8c27efe93b600024ff0b88658bdbc0
Nora Dixon <noradixon03@gmail.com>
2018-04-24 15:40:49 -0500

    Fixed the skills screen

--- app/src/main/java/com/example/andriod/computerglitzapp/Skills.java
+++ app/src/main/java/com/example/andriod/computerglitzapp/Skills.java
@@ -46,24 +46,158 @@ public class Skills extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_skills);
-        mRecycler = (RecyclerView) findViewById(R.id.reclycler);
-        mAdapter = new SkillsAdapter();
-        mRecycler.setAdapter(mAdapter);
-        BottomNavigationView navigation = findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
-        Menu a = navigation.getMenu();
-        MenuItem b = a.findItem(R.id.navigation_home);
-        b.setChecked(true);
 
-        final MNCollege mnCollege = (MNCollege)getIntent().getSerializableExtra("CollegeSelected");
-        final Button websiteButton = (Button) findViewById(R.id.view_holder_button);
-        websiteButton.setOnClickListener(new View.OnClickListener(){
+//        mRecycler = (RecyclerView) findViewById(R.id.reclycler);
+//        mAdapter = new SkillsAdapter();
+//        mRecycler.setAdapter(mAdapter);
+//        BottomNavigationView navigation = findViewById(R.id.navigation);
+//        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+//        Menu a = navigation.getMenu();
+//        MenuItem b = a.findItem(R.id.navigation_home);
+//        b.setChecked(true);
+
+//        final MNCollege mnCollege = (MNCollege)getIntent().getSerializableExtra("CollegeSelected");
+//        final Button websiteButton = (Button) findViewById(R.id.view_holder_button);
+//        websiteButton.setOnClickListener(new View.OnClickListener(){
+//            @Override
+//            public void onClick(View view){
+//                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(mnCollege.getWebsite()));
+//                startActivity(browserIntent);
+//            }
+//        });
+
+        final Button interstButton = findViewById(R.id.intrestButton);
+        interstButton.setOnClickListener (new View.OnClickListener() {
             @Override
-            public void onClick(View view){
-                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(mnCollege.getWebsite()));
-                startActivity(browserIntent);
+            public void onClick(View view) {Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_VIEW);
+                intent.addCategory(Intent.CATEGORY_BROWSABLE);
+                intent.setData(Uri.parse("https://careerwise.minnstate.edu/careers/interestassessment.html"));
+                startActivity(intent);
             }
         });
+
+        final Button clusterButton = findViewById(R.id.clusterButton);
+        clusterButton.setOnClickListener (new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_VIEW);
+                intent.addCategory(Intent.CATEGORY_BROWSABLE);
+                intent.setData(Uri.parse("https://careerwise.minnstate.edu/careers/clusterSurvey?do=0"));
+                startActivity(intent);
+            }
+        });
+
+        final Button seekButton = findViewById(R.id.seekButton);
+        seekButton.setOnClickListener (new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_VIEW);
+                intent.addCategory(Intent.CATEGORY_BROWSABLE);
+                intent.setData(Uri.parse("http://www.iseek.org/careers/stem.html"));
+                startActivity(intent);
+            }
+        });
+
+        final Button gpsButton = findViewById(R.id.gpsButton);
+        gpsButton.setOnClickListener (new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_VIEW);
+                intent.addCategory(Intent.CATEGORY_BROWSABLE);
+                intent.setData(Uri.parse("https://mygpslifeplan.org/"));
+                startActivity(intent);
+            }
+        });
+
+        final Button exploreButton = findViewById(R.id.exploreButton);
+        exploreButton.setOnClickListener (new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_VIEW);
+                intent.addCategory(Intent.CATEGORY_BROWSABLE);
+                intent.setData(Uri.parse("https://www.bls.gov/k12/content/students/careers/career-exploration.htm"));
+                startActivity(intent);
+            }
+        });
+
+        final Button adviceButton = findViewById(R.id.adviceButton);
+        adviceButton.setOnClickListener (new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_VIEW);
+                intent.addCategory(Intent.CATEGORY_BROWSABLE);
+                intent.setData(Uri.parse("https://www.monster.com/career-advice/article/best-free-career-assessment-tools"));
+                startActivity(intent);
+            }
+        });
+
+        final Button top30Button = findViewById(R.id.top30Button);
+        top30Button.setOnClickListener (new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_VIEW);
+                intent.addCategory(Intent.CATEGORY_BROWSABLE);
+                intent.setData(Uri.parse("https://www.onlinecollegeplan.com/vocational-school-highest-paying-careers/"));
+                startActivity(intent);
+            }
+        });
+
+        final Button handbookButton = findViewById(R.id.handbookButton);
+        handbookButton.setOnClickListener (new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_VIEW);
+                intent.addCategory(Intent.CATEGORY_BROWSABLE);
+                intent.setData(Uri.parse("https://www.bls.gov/ooh/"));
+                startActivity(intent);
+            }
+        });
+
+        final Button payButton = findViewById(R.id.payButton);
+        payButton.setOnClickListener (new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_VIEW);
+                intent.addCategory(Intent.CATEGORY_BROWSABLE);
+                intent.setData(Uri.parse("https://www.trade-schools.net/articles/highest-paying-jobs-without-degree.asp"));
+                startActivity(intent);
+            }
+        });
+
+        final Button high43Button = findViewById(R.id.high43Button);
+        high43Button.setOnClickListener (new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_VIEW);
+                intent.addCategory(Intent.CATEGORY_BROWSABLE);
+                intent.setData(Uri.parse("https://www.trade-schools.net/articles/trade-school-jobs.asp "));
+                startActivity(intent);
+            }
+        });
+
+        final Button americaButton = findViewById(R.id.americaButton);
+        americaButton.setOnClickListener (new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_VIEW);
+                intent.addCategory(Intent.CATEGORY_BROWSABLE);
+                intent.setData(Uri.parse("https://www.trade-schools.net/articles/trade-school-jobs.asp"));
+                startActivity(intent);
+            }
+        });
+
+        final Button blueButton = findViewById(R.id.blueButton);
+        blueButton.setOnClickListener (new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_VIEW);
+                intent.addCategory(Intent.CATEGORY_BROWSABLE);
+                intent.setData(Uri.parse("https://www.gcflearnfree.org/careers-without-college/is-a-bluecollar-career-right-for-you/1/"));
+                startActivity(intent);
+            }
+        });
+
 
     }
 
